c2648aecc95eaa160dd40c81a2891f223406950d
Praveen Patro <praveen.patro911@gmail.com>
2023-09-08 16:50:03 +0530

    Update home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import Section from './Section'
+import Section1 from './Section1'
+import Section2 from './Section2'
+import Section3 from './Section3'
+import Section4 from './Section4'
+import Section5 from './Section5'
 const Home = () => {
   return (
     <div>
-        <Section title="Welcome to the Future of Agriculture!" content="Explore, Learn, and Grow with Our Innovative Agricultural Chatbot. Your Trusted Partner in Farming Knowledge, news, latest market prices and Insights." image="https://m.economictimes.com/thumb/msid-77969178,width-1200,height-900,resizemode-4,imgsize-727760/agri-tech.jpg" alt="field"/>
+        <Section1 title="Welcome to the Future of Agriculture!" content="Explore, Learn, and Grow with Our Innovative Agricultural Chatbot. Your Trusted Partner in Farming Knowledge, news, latest market prices and Insights." image="https://m.economictimes.com/thumb/msid-77969178,width-1200,height-900,resizemode-4,imgsize-727760/agri-tech.jpg" alt="field"/>
+        <Section2 title="AI-Powered ChatBOT" />
+        <Section3 title="Active Community"/>
+        <Section4 title="Groundbreaking Innovations"/>
+        <Section5 title="Real-Time Market prices"/>
     </div>
   )
 }
---
+++ src/Components/Home/Section1.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./home.css";
+const Section1 = (props) => {
+    return (
+      <div className="mx-0 my-0  h-screen flex section">
+        <div className="self-center justify-start">
+          <h1 className="px-10 py-10 text-left text-6xl font-semibold transition ease-in-out delay-150 hover:translate-x-1 hover:translate-y-1 duration-200">{props.title}</h1>
+          <div><p className="px-10 py-0 text-left text-lg font-medium">{props.content}</p></div>
+          </div>
+          <img className="mr-12 my-20 border-solid border-4 rounded-full border-white h-80 w-80 self-center justify-end transition ease-in-out delay-300 hover:-translate-y-1 hover:scale-110 duration-300" src={props.image} alt={props.alt} />
+      </div>
+    )
+  }
+
+export default Section1;
